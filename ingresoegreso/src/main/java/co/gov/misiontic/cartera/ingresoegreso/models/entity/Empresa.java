bcc9d7959eaaef7725c5a570912f49ef8422e18e
JohanHer <johan.hernandezv@udea.edu.co>
2022-08-23 11:26:08 -0500

    Se añade las dependencias lombok y JPA, ademas se crea la clase entidad Empresa

---
+++ ingresoegreso/src/main/java/co/gov/misiontic/cartera/ingresoegreso/models/entity/Empresa.java
@@ -0,0 +1,14 @@
+package co.gov.misiontic.cartera.ingresoegreso.models.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+public class Empresa {
+    private String nombre;
+    private String direccion;
+    private String telefoto;
+    private String NIT;
+}
