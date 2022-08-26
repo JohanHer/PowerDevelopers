b359b81f43539e3a37ab4a3c9b77299637eca40b
Edgar Ladino <edilad@hotmail.com>
2022-08-26 09:04:26 -0500

    Clase empresa

--- ingresoegreso/src/main/java/co/gov/misiontic/cartera/ingresoegreso/models/entity/empresa/Empresa.java
+++ ingresoegreso/src/main/java/co/gov/misiontic/cartera/ingresoegreso/models/entity/empresa/Empresa.java
@@ -1,14 +1,18 @@
-package co.gov.misiontic.cartera.ingresoegreso.models.entity.empresa;
+package co.gov.misiontic.cartera.ingresoegreso.models.entity;
 
+import co.gov.misiontic.cartera.ingresoegreso.models.entity.empresa.Localizacion;
 import lombok.Data;
 
-//import javax.persistence.Entity;
+//import javax.persistence.*;
 
 //@Entity
-@Data
+//@Table(name="Empresa") //Entidad enlazada a una tabla
+@Data // Genera los setter and getter y constructor
 public class Empresa {
+    // @Id //Llave primaria
+    // @GeneratedValue(strategy = GenerationType.AUTO) //Para que se autoincremente
     private String nombre;
-    private String direccion;
+    private Localizacion direccion;
     private String telefoto;
     private String NIT;
 }
---
+++ ingresoegreso/src/main/java/co/gov/misiontic/cartera/ingresoegreso/models/entity/empresa/Localizacion.java
@@ -0,0 +1,12 @@
+package co.gov.misiontic.cartera.ingresoegreso.models.entity.empresa;
+
+import lombok.Data;
+
+@Data
+public class Localizacion {
+    private String Direccion;
+    private String Ciudad;
+    private String Departamento;
+    private String Pais;
+
+}
